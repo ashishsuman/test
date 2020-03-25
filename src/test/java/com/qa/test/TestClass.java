b632ca96d3c9260b9b8e5cd70506a9339b223689
ashishsuman <ashishsuman91@gmail.com>
2020-03-25 20:02:31 +0530

    First commit

---
+++ src/test/java/com/qa/test/TestClass.java
@@ -0,0 +1,15 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	
+	public void testExample1(){
+		
+		System.out.println("Example 1");
+		
+	}
+
+}
